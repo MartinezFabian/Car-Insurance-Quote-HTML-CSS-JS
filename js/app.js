054654b7ae9d6a9c7885cff9d4379c7cae6078d4
MartinezFabian <FabianMartinezDev@gmail.com>
2023-05-21 19:29:50 -0300

    feat: Add functionality for insurance quote

--- js/app.js
+++ js/app.js
@@ -5,6 +5,54 @@ function Insurance(brand, year, type) {
   this.type = type;
 }
 
+//cotizar seguro
+Insurance.prototype.quote = function (basePrice) {
+  /*
+    1 = Americano   1.15
+    2 =  Asiático   1.05 
+    3 = Europeo     1.35
+  */
+
+  let finalPrice = 0;
+
+  switch (this.brand) {
+    case "1":
+      finalPrice = basePrice * 1.15;
+      break;
+
+    case "2":
+      finalPrice = basePrice * 1.05;
+      break;
+
+    case "3":
+      finalPrice = basePrice * 1.35;
+      break;
+
+    default:
+      break;
+  }
+
+  // por cada año de antigüedad del automóvil el precio se reduce en un 3%
+
+  //conseguimos el año actual
+  let currentYear = new Date().getFullYear();
+
+  //le restamos al año actual el año del automóvil
+  let difference = currentYear - this.year;
+
+  // Reducción del precio en función de la antigüedad del automóvil
+  finalPrice -= (difference * 3 * finalPrice) / 100;
+
+  /*
+    Seguro de tipo básico: 30% mas
+    Seguro de tipo completo: 50% mas 
+  */
+
+  finalPrice = this.type === "basic" ? finalPrice * 1.3 : this.type === "full" ?? finalPrice * 1.5;
+
+  return finalPrice;
+};
+
 function UserInterface() {}
 
 // llenar el select year con opciones de años desde el año actual hasta 20 años atrás
@@ -41,7 +89,7 @@ UserInterface.prototype.showMessage = (message, type) => {
 
     form.insertBefore(div, form.querySelector("#form__result"));
 
-    //luego de 3 segundos eliminar el mensaje
+    //luego de 2 segundos eliminar el mensaje
     setTimeout(() => {
       div.remove();
     }, 2000);
@@ -88,5 +136,11 @@ function main() {
     //si todos los campos están seleccionados
 
     ui.showMessage("Cotizando...", "success");
+
+    const insurance = new Insurance(selectBrand, selectYear, typeInsurance);
+
+    //le pasamos el precio base
+    let result = insurance.quote(2000);
+    console.log(result);
   }
 }
